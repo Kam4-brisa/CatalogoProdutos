51c57ec20c66d45f24f3c2209cc31e2411dbcde9
Pelife12 <felipebogojgs@gmail.com>
2025-10-03 20:24:35 -0300

    WIP

---
+++ src/main/java/com/example/CatalogoProdutos/CatalogoProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.example.CatalogoProdutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoProdutosApplication.class, args);
+	}
+
+}
